19628226a1d109de88acfb2925c7712b05f2b811
Mae Yee <mae.cast@hotmail.com>
2015-04-02 13:49:28 -0700

    * fixed sessions.sql script -> table name should be ci_session in the singular * enable sessions Signed-off-by:Mae Yee <mae.cast@hotmail.com>

--- data/sessions.sql
+++ data/sessions.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `ci_sessions` (
+CREATE TABLE IF NOT EXISTS `ci_session` (
         `id` varchar(40) NOT NULL,
         `ip_address` varchar(45) NOT NULL,
         `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
